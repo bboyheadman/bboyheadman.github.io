8db0dc7f913e4d574863f28b6ca73e536eb16d8a
gebeto <bboyheadman@gmail.com>
2017-03-29 14:12:20 +0300

    QuickCommit

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
 
 
 window.onload = function() {
+	
+
+	document.getElementById('login').addEventListener('click', login);
+	document.getElementById('logout').addEventListener('click', logout);
+
+}
+
+
+
+function login() {
+	VK.Auth.login(function(authData) {
+		console.log(authData);
+		localStorage.setItem('auth', JSON.stringify(authData.session));
+		// alert('Авторизирован как ' + authData.session.user.first_name + ' ' + authData.session.user.last_name);
+		var auth = JSON.parse(localStorage.getItem('auth'));
+		document.getElementById('auth-status').textContent = ('Авторизирован как ' + auth.user.first_name + ' ' + auth.user.last_name);
+	}, 8192)
+}
+
+function logout() {
+	if (confirm('Вы действительно хотите выйти?')) {
+		VK.Auth.logout(function(authData) {
+			localStorage.setItem('auth', '');
+			console.log(authData);
+		});
+		document.getElementById('auth-status').textContent = 'Авторизируйтесь';
+	}
+}
+
+function authCheck() {
 	if (localStorage.getItem('auth')) {
 		var auth = JSON.parse(localStorage.getItem('auth'));
 		console.log(auth);
-		document.getElementById('auth-status').textContent = ('Авторизирован как ' + auth.user.first_name + ' ' + auth.user.last_name);
+		document.querySelector('title').textContent = ('Авторизирован как ' + auth.user.first_name + ' ' + auth.user.last_name);
 	} else {
-		document.getElementById('auth-status').textContent = 'Авторизируйтесь';
+		document.querySelector('title').textContent = 'Авторизируйтесь';
 	}
-
-	document.getElementById('login').addEventListener('click', function() {
-		VK.Auth.login(function(authData) {
-			console.log(authData);
-			localStorage.setItem('auth', JSON.stringify(authData.session));
-			// alert('Авторизирован как ' + authData.session.user.first_name + ' ' + authData.session.user.last_name);
-			var auth = JSON.parse(localStorage.getItem('auth'));
-			document.getElementById('auth-status').textContent = ('Авторизирован как ' + auth.user.first_name + ' ' + auth.user.last_name);
-		}, 8192)
-	});
-
-	document.getElementById('logout').addEventListener('click', function() {
-		if (confirm('Вы действительно хотите выйти?')) {
-			VK.Auth.logout(function(authData) {
-				localStorage.setItem('auth', '');
-				console.log(authData);
-			});
-			document.getElementById('auth-status').textContent = 'Авторизируйтесь';
-		}
-	});
 }
