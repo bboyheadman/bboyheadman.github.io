4761cc9b8ffed1fcfb8c2c959e5ae41fd446f3e1
gebeto <bboyheadman@gmail.com>
2017-03-29 16:37:15 +0300

    QuickCommit

--- Components/GroupListItem.js
+++ Components/GroupListItem.js
@@ -11,13 +11,20 @@ function GroupListItem(groupInfo) {
 GroupListItem.prototype.createElement = function() {
 	var container = document.createElement('div');
 	container.style.width = '300px';
-	container.style.height = '100px';
+	container.style.height = '80px';
 	container.style.background = '#eee';
+	container.style.marginTop = '3px';
+
+	var itemPhoto = document.createElement('img');
+	itemPhoto.src = this.avatar;
+	itemPhoto.style.height = '100%';
+
 	
 	var spanTitle = document.createElement('span');
-	spanTitle.style.font: '1em Arial';
+	spanTitle.style.font = '1em Arial';
 	spanTitle.textContent = this.name;
 	
+	container.appendChild(itemPhoto);
 	container.appendChild(spanTitle);
 
 	return container;
