d47928f305b7c778b9c5876b43783f36bf760673
gebeto <bboyheadman@gmail.com>
2017-03-29 11:28:18 +0300

    QuickCommit

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 
 
 window.onload = function() {
-	document.getElementById('authorization').addEventListener('click', function() {
+	document.getElementById('login').addEventListener('click', function() {
 		VK.Auth.login(function(authData) {
 			console.log(authData);
 			localStorage.setItem('auth', authData);
 		}, 8192)
+	});
+	document.getElementById('logout').addEventListener('click', function() {
+		VK.Auth.logout(function(authData) {
+			console.log(authData);
+			localStorage.setItem('auth', null);
+		});
 	});
 }
