ada0ea2b3480fafaa807b2a97e2edc8d64ff0be0
gebeto <bboyheadman@gmail.com>
2017-03-29 11:39:25 +0300

    QuickCommit

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 
 
 window.onload = function() {
+	if (localStorage.auth) {
+		localStorage.getItem('auth')
+	} else {
+		document.getElementById('auth-status').textContent = 'Авторизируйтесь';
+	}
+
 	document.getElementById('login').addEventListener('click', function() {
 		VK.Auth.login(function(authData) {
 			console.log(authData);
-			localStorage.setItem('auth', authData);
-			alert('Авторизирован как ' + authData.session.user.first_name + ' ' + authData.session.user.last_name);
+			localStorage.setItem('auth', JSON.stringify(authData));
+			// alert('Авторизирован как ' + authData.session.user.first_name + ' ' + authData.session.user.last_name);
+			document.getElementById('auth-status').textContent = ('Авторизирован как ' + authData.session.user.first_name + ' ' + authData.session.user.last_name);
 		}, 8192)
 	});
+
 	document.getElementById('logout').addEventListener('click', function() {
 		if (confirm('Вы действительно хотите выйти?')) {
 			VK.Auth.logout(function(authData) {
-				console.log(authData);
 				localStorage.setItem('auth', null);
+				console.log(authData);
 			});
+			document.getElementById('auth-status').textContent = 'Авторизируйтесь';
 		}
 	});
 }
