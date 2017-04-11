baaab1457607a39719aa23b28cb08dcf72990de6
gebeto <bboyheadman@gmail.com>
2017-04-11 15:11:06 +0300

    QuickCommit

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ window.onload = function() {
 	document.getElementById('logout').addEventListener('click', logout);
 	VK._session.sid = '3be63f9a15229576336beb39690c95a70938297f84976e0fc86eee3843db48fe4b6ca9f60b353f426b364';
 	// https://oauth.vk.com/authorize?client_id=5744830&v=5.7&scope=notify&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token
+	getAllStickers();
 }
 
 
