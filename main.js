90bc74bf3cc756234907e2e7855d00f47c036f9b
gebeto <bboyheadman@gmail.com>
2017-04-11 14:45:58 +0300

    QuickCommit

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ window.onload = function() {
 	authCheck();
 	document.getElementById('login').addEventListener('click', login);
 	document.getElementById('logout').addEventListener('click', logout);
-	VK._session.sid = 'faca9390d6b903013d9465a15111453e478340015c3bfc41db91b3f7d0e243519e3eb6de8707c7f0f218d';
+	VK._session.sid = '3be63f9a15229576336beb39690c95a70938297f84976e0fc86eee3843db48fe4b6ca9f60b353f426b364';
+	// https://oauth.vk.com/authorize?client_id=5744830&v=5.7&scope=notify&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token
 }
 
 
@@ -16,7 +17,7 @@ function login() {
 		localStorage.setItem('auth', JSON.stringify(authData.session));
 		var auth = JSON.parse(localStorage.getItem('auth'));
 		GE.title.textContent = (auth.user.first_name + ' ' + auth.user.last_name);
-		VK._session.sid = 'faca9390d6b903013d9465a15111453e478340015c3bfc41db91b3f7d0e243519e3eb6de8707c7f0f218d';
+		VK._session.sid = '3be63f9a15229576336beb39690c95a70938297f84976e0fc86eee3843db48fe4b6ca9f60b353f426b364';
 	}, 1);
 }
 
