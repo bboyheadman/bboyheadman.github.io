1e15db8b575df373b8a4fde94d9073887a0da029
gebeto <bboyheadman@gmail.com>
2017-04-19 11:37:29 +0300

    QuickCommit

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ window.onload = function() {
 	authCheck();
 	document.getElementById('login').addEventListener('click', login);
 	document.getElementById('logout').addEventListener('click', logout);
-	VK._session.sid = '3be63f9a15229576336beb39690c95a70938297f84976e0fc86eee3843db48fe4b6ca9f60b353f426b364';
 	// https://oauth.vk.com/authorize?client_id=5744830&v=5.7&scope=notify&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token
 	getAllStickers();
 }
@@ -22,7 +21,6 @@ function login() {
 		localStorage.setItem('auth', JSON.stringify(authData.session));
 		var auth = JSON.parse(localStorage.getItem('auth'));
 		GE.title.textContent = (auth.user.first_name + ' ' + auth.user.last_name);
-		VK._session.sid = '3be63f9a15229576336beb39690c95a70938297f84976e0fc86eee3843db48fe4b6ca9f60b353f426b364';
 	}, 1);
 }
 
